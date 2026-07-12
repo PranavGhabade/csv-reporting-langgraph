fc030f0bf14b4a51816979714e7201fd9a0e651f
PranavGhabade <pranavghabade97@gmail.com>
2026-07-12 21:10:44 +0530

    Update llm for Streamlit deployment

--- llm.py
+++ llm.py
@@ -5,8 +5,20 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 
 load_dotenv()
 
+try:
+    import streamlit as st
+
+    api_key = st.secrets.get(
+        "GOOGLE_API_KEY",
+        os.getenv("GOOGLE_API_KEY")
+    )
+
+except Exception:
+    api_key = os.getenv("GOOGLE_API_KEY")
+
+
 llm = ChatGoogleGenerativeAI(
     model="gemini-3.5-flash",
     temperature=0,
-    google_api_key=os.getenv("GOOGLE_API_KEY")
+    google_api_key=api_key
 )
